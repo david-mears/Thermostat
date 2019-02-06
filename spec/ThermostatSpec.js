0f50cb5abcb383f9d4247d5f3f85c5edc9ee598a
david-mears <kieukmail@gmail.com>
2019-02-06 17:03:33 +0000

    Equivalent test for #down

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -9,13 +9,22 @@ describe('Thermostat', function() {
   it('has a default temperature of 20', function() {
     expect(thermo.temp).toEqual(20)
   });
-  
+
   describe ('#up', function() {
-    it('up increases temp by 1', function() {
+    it('increases temp by 1', function() {
       temp1 = thermo.temp;
       thermo.up();
       temp2 = thermo.temp;
       expect(temp2 - temp1).toEqual(1)
     });
-  });  
+  });
+
+  describe ('#down', function() {
+    it('decreases temp by 1', function() {
+      temp1 = thermo.temp;
+      thermo.down();
+      temp2 = thermo.temp;
+      expect(temp2 - temp1).toEqual(-1)
+    });
+  });
 });
