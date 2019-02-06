0391f56f3757fa58bf88aa48c09eefc9bce5993e
Erin Stevenson <estevenson_94@live.com>
2019-02-06 16:59:01 +0000

    Created test for thermo.up

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,6 +1,6 @@
 describe('Thermostat', function() {
 
-  var thermostat;
+  var thermo;
 
   beforeEach(function() {
     thermo = new Thermostat
@@ -9,5 +9,13 @@ describe('Thermostat', function() {
   it('has a default temperature of 20', function() {
     expect(thermo.temp).toEqual(20)
   });
-
+  
+  describe ('#up', function() {
+    it('up increases temp by 1', function() {
+      temp1 = thermo.temp;
+      thermo.up();
+      temp2 = thermo.temp;
+      expect(temp2 - temp1).toEqual(1)
+    });
+  });  
 });
